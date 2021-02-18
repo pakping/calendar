001829778a6c42ff0c75f473522d375e22b9f0b7
jee <guitar_104@hotmail.com>
2021-02-18 13:58:09 +0700

    nav ปรับจาก bootstarp to buma

--- app/home.php
+++ app/home.php
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-  <link href="../css/navbar.css" rel="stylesheet" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
   <title>UP</title>
 </head>
 
--- components/nav.php
+++ components/nav.php
@@ -1,4 +1,90 @@
-<nav class="navbar navbar-light" style = " background-color:#6610f2;">
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar" style="width: 100%; height: 75px; background-color: #673ab7;">
+    <h1 style="margin: 0 auto; padding: 25px; color: white;">
+      โรงพยาบาลมหาลัยพะเยา
+    </h1>
+  </div>
+</nav>
+
+<nav class="navbar is-warning" role="navigation" aria-label="main navigation" style="height: 75px;">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar is-warning">
+      <a class="navbar-item">
+        หน้าหลัก
+      </a>
+
+      <a class="navbar-item">
+        ห้องประชุม
+      </a>
+      <a class="navbar-item">
+        ตารางปฏิทิน
+      </a>
+      <a class="navbar-item">
+        จองห้อง
+      </a>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            คุณ 
+          </a>
+
+          <div class="navbar-dropdown ">
+            <!-- Other navbar items -->
+            <form action="" method="post">
+              
+              <button type="submit" class="button">ออกจากระบบ</button>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</nav>
+
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
+
+  });
+</script>
+<!-- <nav class="navbar navbar-light" style = " background-color:#6610f2;">
   <div class="container-fluid justify-content-md-center">
     <span class="navbar-text">
       <h5 class="textnavbar md-4 ">ระบบจองห้องประชุมโรงพยาบาลมหาวิทยาลัยพะเยา</h5>
@@ -162,4 +248,4 @@ ul {
     color: #fff;
   
 }
-</style>
+</style> -->
