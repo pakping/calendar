c1c4f0b5c7c534a9058b8fe970cb1cc577093740
pakping <pakping-up@hotmai.com>
2021-03-04 14:53:53 +0700

    no message

--- admin/view-room.php
+++ admin/view-room.php
@@ -14,66 +14,65 @@ include '../auth/Sessionpersist.php';
 </head>
 
 <body>
-<?php
+  <?php
   include '../components/navbaradmin.php';
   ?>
-<section class="section">
-    <div class="container">
-      <h3 class="title"> ห้องประชุม</h3>
-    </div>
-  </section>
-  
+
   <section class="section">
-    <h2 class="title is-size-6-mobile is-size-2-tablet"></h2>
+  <div class="box has-background-light">
+    <h2 class="title is-size-6-mobile is-size-2-tablet">ห้องประชุม</h2>
     <div class="columns is-mobile is-multiline is-variable is-1">
-<?php
-    require "../DB/connect.php";
+      <?php
+      require "../DB/connect.php";
       $Squery = "SELECT * FROM room";
-        if ($result = mysqli_query($con, $Squery)) {
-          while ($room = mysqli_fetch_array($result)) {
-?>
-      <div class="column is-12-mobile is-4-tablet is-3-desktop">
-        <div class="box has-background-warning">
-          <div class="card">
-            <header class="header">
-              <p class=" title is-6 card-header-title "><?php echo $room['roomname'] ?></p>
-            </header>
-            <div class="image">
-              <figure class="image is-4by3">
-                <img src=<?php echo $room['roomimg']; ?> alt="Placeholder image">
-              </figure>
-            </div>
-            <div class="card-content">
-              <div class="content">
-                <div class="tags are-medium">
-                <?php  if ($room['com']!='0'){ ?>
-                  <span class="tag is-normal is-primary">คอมพิวเตอร์</span>
-                <?php }if($room['screen']!='0'){ ?>
-                  <span class="tag is-normal is-link">จอทีวี</span>
-                <?php }if ($room['mic']!= '0'){  ?>
-                  <span class="tag is-normal is-purple">ไมโครโฟน</span>
-                <?php } ?>
-                  <span class="tag is-normal is-danger">รับได้ <?php echo $room['roomcap']  ?> ท่าน</span>
+      if ($result = mysqli_query($con, $Squery)) {
+        while ($room = mysqli_fetch_array($result)) {
+      ?>
+          <div class="column is-12-mobile is-4-tablet is-3-desktop">
+        
+              <div class="card">
+                <header class="header">
+                  <p class=" title is-6 card-header-title "><?php echo $room['roomname'] ?></p>
+                </header>
+                <div class="image">
+                  <figure class="image is-4by3">
+                    <img src=<?php echo $room['roomimg']; ?> alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="content">
+                    <div class="tags are-medium">
+                      <?php if ($room['com'] != '0') { ?>
+                        <span class="tag is-normal is-primary">คอมพิวเตอร์</span>
+                      <?php }
+                      if ($room['screen'] != '0') { ?>
+                        <span class="tag is-normal is-link">จอทีวี</span>
+                      <?php }
+                      if ($room['mic'] != '0') {  ?>
+                        <span class="tag is-normal is-purple">ไมโครโฟน</span>
+                      <?php } ?>
+                      <span class="tag is-normal is-danger">รับได้ <?php echo $room['roomcap']  ?> ท่าน</span>
+                    </div>
+                    <br>
+                    <!-- <time datetime="2016-1-1"><a href="#"># PM - 1 Jan 2021</a></time> -->
                   </div>
-                <br>
-                <!-- <time datetime="2016-1-1"><a href="#"># PM - 1 Jan 2021</a></time> -->
-              </div>
-            </div>
+                </div>
 
-            <form action="Edit-room.php" method="POST" >
-            <div class="box has-text-centered">
-              <button    class="button is-primary  " type="submit" href="#" >Edit/Delete</button>
-              <input type="hidden" name='sentid' value="<?php echo $room['roomid'];?>">
-              </div>
-            </form>
-          
+                <form action="Edit-room.php" method="POST">
+                  <div class="box has-text-centered">
+                    <button class="button is-primary  " type="submit" href="#">Edit/Delete</button>
+                    <input type="hidden" name='sentid' value="<?php echo $room['roomid']; ?>">
+                  </div>
+                </form>
 
+                        
+              </div>
+            
           </div>
-        </div>
-      </div>
-<?php
-          }}
-?>
+      <?php
+        }
+      }
+      ?>
 
 
     </div>
