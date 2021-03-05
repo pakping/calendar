bfc8aa79d07fa08a91ca9fb60373de12187edb2b
jee <guitar_104@hotmail.com>
2021-03-05 15:02:23 +0700

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,56 @@
-<footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-    </p>
+<br>
+<footer class="footer has-background-dark has-text-primary-light">
+  <div class="container">
+    <div class="content has-text-centered">
+      <div class="columns is-desktop">
+        <div class="column is-one-quarter"><img style="width: 100%;" src="http://upmed.up.ac.th/images/logo/footer-logo.png" alt="Logo Image"></div>
+        <div class="column is-one-quarter">
+          <div class="icon-text">
+            <span class="icon has-text-warning">
+              <i class="far fa-map" style="font-size: 18px;"></i>
+            </span>
+            <span>โรงพยาบาลมหาวิทยาลัยพะเยา <br> คณะแพทยศาสตร์</span>
+          </div>
+          <p class="block">
+            19/1 หมู่ 2 ถ.พหลโยธิน ต.แม่กา <br> อ.เมืองพะเยา จ.พะเยา 56000
+          </p>
+        </div>
+        <div class="column is-one-quarter">
+          <div class="icon-text">
+            <span class="icon has-text-warning">
+              <i class="fas fa-phone-alt"></i>
+            </span>
+            <span>+66-5446-6666 ต่อ 7000</span>
+          </div>
+          <div class="icon-text">
+            <span class="icon has-text-warning">
+              <i class="fas fa-headset"></i>
+            </span>
+            <span>ฉุกเฉิน : +66-5446-6757</span>
+          </div>
+        </div>
+        <div class="column is-one-quarter">
+          <div class="icon-text">
+            <span class="icon has-text-warning">
+              <i class="fas fa-fax"></i>
+            </span>
+            <span>โทรสาร : 054466759 </span>
+          </div>
+          <div class="icon-text">
+            <span class="icon has-text-warning">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span>Email : upmed@up.ac.th</span>
+          </div>
+        </div>
+      </div>
+      <br>
+      <p>
+        <strong class="has-text-primary-light"><i class="fab fa-facebook" style="font-size: 32px;"></i></strong>  <a href="https://www.facebook.com/UPHospital">โรงพยาบาล มหาวิทยาลัยพะเยา คณะแพทยศาสตร์</a>
+        <hr>
+      <p>Copyright © 2021 UPH. All Rights Reserved.</p>
+      </p>
+    </div>
   </div>
 </footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js"></script>
