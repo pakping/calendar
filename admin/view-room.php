1e4eacb58d7b3feb0cd581ccf55ad169f78c63fa
Uphplocal <up.localhp@gmail.com>
2021-03-05 08:52:57 +0700

    q

--- admin/view-room.php
+++ admin/view-room.php
@@ -22,34 +22,39 @@ include '../auth/Sessionpersist.php';
   <div class="box has-background-light">
     <h2 class="title is-size-6-mobile is-size-2-tablet">ห้องประชุม</h2>
     <div class="columns is-mobile is-multiline is-variable is-1">
-<?php
-    require "../DB/connect.php";
-      $Squery = "SELECT * FROM room Order by roomname";
-        if ($result = mysqli_query($con, $Squery)) {
-          while ($room = mysqli_fetch_array($result)) {
-?>
-      <div class="column is-12-mobile is-4-tablet is-3-desktop">
-        <div class="box has-background-warning">
-          <div class="card">
-            <header class="header">
-              <p class=" title is-6 card-header-title "><?php echo $room['roomname'] ?></p>
-            </header>
-            <div class="image">
-              <figure class="image is-4by3">
-                <img src=<?php echo $room['roomimg']; ?> alt="Placeholder image">
-              </figure>
-            </div>
-            <div class="card-content">
-              <div class="content">
-                <div class="tags are-medium">
-                <?php  if ($room['com']!='0'){ ?>
-                  <span class="tag is-normal is-primary">คอมพิวเตอร์</span>
-                <?php }if($room['screen']!='0'){ ?>
-                  <span class="tag is-normal is-link">จอทีวี</span>
-                <?php }if ($room['mic']!= '0'){  ?>
-                  <span class="tag is-normal is-purple">ไมโครโฟน</span>
-                <?php } ?>
-                  <span class="tag is-normal is-danger">รับได้ <?php echo $room['roomcap']  ?> ท่าน</span>
+      <?php
+      require "../DB/connect.php";
+      $Squery = "SELECT * FROM room Order By roomname";
+      if ($result = mysqli_query($con, $Squery)) {
+        while ($room = mysqli_fetch_array($result)) {
+      ?>
+          <div class="column is-12-mobile is-4-tablet is-3-desktop">
+        
+              <div class="card">
+                <header class="header">
+                  <p class=" title is-6 card-header-title "><?php echo $room['roomname'] ?></p>
+                </header>
+                <div class="image">
+                  <figure class="image is-4by3">
+                    <img src=<?php echo $room['roomimg']; ?> alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="content">
+                    <div class="tags are-medium">
+                      <?php if ($room['com'] != '0') { ?>
+                        <span class="tag is-normal is-primary">คอมพิวเตอร์</span>
+                      <?php }
+                      if ($room['screen'] != '0') { ?>
+                        <span class="tag is-normal is-link">จอทีวี</span>
+                      <?php }
+                      if ($room['mic'] != '0') {  ?>
+                        <span class="tag is-normal is-purple">ไมโครโฟน</span>
+                      <?php } ?>
+                      <span class="tag is-normal is-danger">รับได้ <?php echo $room['roomcap']  ?> ท่าน</span>
+                    </div>
+                    <br>
+                    <!-- <time datetime="2016-1-1"><a href="#"># PM - 1 Jan 2021</a></time> -->
                   </div>
                 </div>
 
