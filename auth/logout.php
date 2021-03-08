fd9205e1f8b2a5aa9908a7ca193be9a97c09a777
Uphplocal <up.localhp@gmail.com>
2021-03-08 10:26:09 +0700

    fix

--- auth/logout.php
+++ auth/logout.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	require_once("../DB/connnet.php");
+	require_once("../DB/connect.php");
 
 	//*** Update Status
 	$sql = "UPDATE User SET LoginStatus = '0', LastUpdate = '0000-00-00 00:00:00' WHERE Username = '".$_SESSION["Username"]."' ";
