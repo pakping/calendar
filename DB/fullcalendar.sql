72a49fbb29e1473818378f1d03623e84ffee05c1
Uphplocal <up.localhp@gmail.com>
2021-03-09 16:45:24 +0700

    sdasd

--- DB/fullcalendar.sql
+++ DB/fullcalendar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2021 at 08:17 AM
+-- Generation Time: Mar 09, 2021 at 10:44 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -24,23 +24,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `cars_id` int(3) NOT NULL,
+  `cars_type` varchar(30) NOT NULL,
+  `cars_name` varchar(60) NOT NULL,
+  `bgcolor` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`cars_id`, `cars_type`, `cars_name`, `bgcolor`) VALUES
+(1, 'ตู้', 'นข 3421 พย.', '#4ff20f'),
+(2, 'ตู้', 'นข 3455 พย.', '#0d06d4'),
+(3, 'ตู้', 'นข 3460 พย.', '#fa02d9'),
+(4, 'เก๋ง', 'กต 3607 พย.', '#e1ff00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `cars_event`
 --
 
 CREATE TABLE `cars_event` (
   `event_id` int(3) NOT NULL,
+  `cars_id` int(3) NOT NULL,
   `event_title` varchar(80) NOT NULL,
   `event_detail` text NOT NULL,
   `event_startdate` date NOT NULL,
   `event_enddate` date NOT NULL,
   `event_starttime` time NOT NULL,
+  `event_endtime` time NOT NULL,
   `event_color` varchar(15) NOT NULL DEFAULT '#FFFFFF',
   `event_bgcolor` varchar(15) NOT NULL DEFAULT '#03a9f4',
   `event_createdate` timestamp NOT NULL DEFAULT current_timestamp(),
   `people` int(3) NOT NULL,
-  `statid` int(3) NOT NULL,
-  `regname` varchar(100) NOT NULL
+  `regname` varchar(100) NOT NULL,
+  `agency` varchar(20) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `statid` int(11) DEFAULT 2,
+  `Username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cars_event`
+--
+
+INSERT INTO `cars_event` (`event_id`, `cars_id`, `event_title`, `event_detail`, `event_startdate`, `event_enddate`, `event_starttime`, `event_endtime`, `event_color`, `event_bgcolor`, `event_createdate`, `people`, `regname`, `agency`, `location`, `statid`, `Username`) VALUES
+(1, 1, 'Future of gaming', 'แอ้ว', '2021-03-10', '2021-03-10', '17:38:00', '17:38:00', '#FFFFFF', '#4ff20f', '2021-03-09 08:54:55', 12, 'jeerachon', 'กลัวเมีย', 'หน้ามอ', 2, 'Jeerachon'),
+(1, 2, 'Future of gaming', 'แอ้ว', '2021-03-10', '2021-03-10', '17:38:00', '17:38:00', '#FFFFFF', '#0d06d4', '2021-03-09 08:54:55', 12, 'jeerachon', 'กลัวเมีย', 'หน้ามอ', 2, 'Jeerachon'),
+(1, 3, 'Future of gaming', 'แอ้ว', '2021-03-10', '2021-03-10', '17:38:00', '17:38:00', '#FFFFFF', '#fa02d9', '2021-03-09 08:54:55', 12, 'jeerachon', 'กลัวเมีย', 'หน้ามอ', 2, 'Jeerachon');
 
 -- --------------------------------------------------------
 
@@ -167,7 +204,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`Username`, `Password`, `Lastupdate`, `LoginStatus`, `Access`, `fname`, `lname`, `email`, `pnum`) VALUES
-('jeerachon', 'e10adc3949ba59abbe56e057f20f883e', '0000-00-00 00:00:00', 0, 'user', 'Jeerachon', 'Tummasorn', 'Humgee@Hum.co.th', 123456789),
+('jeerachon', 'e10adc3949ba59abbe56e057f20f883e', '2021-03-09 16:38:29', 0, 'user', 'Jeerachon', 'Tummasorn', 'Humgee@Hum.co.th', 123456789),
 ('Mojung', '81dc9bdb52d04dc20036dbd8313ed055', '0000-00-00 00:00:00', 0, 'user', 'Mr', 'Mojung', 'mojung@jung.com', 999999999),
 ('phoomin', 'e35cf7b66449df565f93c607d5a81d09', '0000-00-00 00:00:00', 0, 'user', 'Phoomin', 'Boonanan', 'Min@hum.co.th', 987654321),
 ('sirichai', 'c33367701511b4f6020ec61ded352059', '0000-00-00 00:00:00', 0, 'admin', 'Sirichai', 'Benjamakom', 'Den@hum.co.th', 696969696);
@@ -177,10 +214,19 @@ INSERT INTO `user` (`Username`, `Password`, `Lastupdate`, `LoginStatus`, `Access
 --
 
 --
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`cars_id`);
+
+--
 -- Indexes for table `cars_event`
 --
 ALTER TABLE `cars_event`
-  ADD PRIMARY KEY (`event_id`);
+  ADD PRIMARY KEY (`event_id`,`cars_id`),
+  ADD KEY `fk_cars` (`cars_id`),
+  ADD KEY `fk_statid2` (`statid`),
+  ADD KEY `fk_username2` (`Username`);
 
 --
 -- Indexes for table `room`
@@ -214,10 +260,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `cars_event`
+-- AUTO_INCREMENT for table `cars`
 --
-ALTER TABLE `cars_event`
-  MODIFY `event_id` int(3) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `cars`
+  MODIFY `cars_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `room`
@@ -240,6 +286,14 @@ ALTER TABLE `tbl_event`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `cars_event`
+--
+ALTER TABLE `cars_event`
+  ADD CONSTRAINT `fk_cars` FOREIGN KEY (`cars_id`) REFERENCES `cars` (`cars_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_statid2` FOREIGN KEY (`statid`) REFERENCES `stat` (`statid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_username2` FOREIGN KEY (`Username`) REFERENCES `user` (`Username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tbl_event`
